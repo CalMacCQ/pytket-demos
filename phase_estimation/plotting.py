a0bf9c7f55f1738ee8b3152804f5a0d4b8876719
CallumCQ <callum.macpherson@cambridgequantum.com>
2022-06-21 11:10:51 +0100

    plotting and code imporvements

--- phase_estimation/plotting.py
+++ phase_estimation/plotting.py
@@ -5,7 +5,7 @@ plt.style.use(["science", "notebook", "grid"])
 
 def tuple_to_string1(tup):
     """
-    Converts a Tuple to a string as follows (1,0,0) -> "(1,0,0)
+    Converts a Tuple to a string as follows (1,0,0) -> "(1,0,0)"
     """
     return str(tup)
 
@@ -27,16 +27,18 @@ def dictionary_sorter(given_dict, rank=6):
     return sorted_dict
 
 
-def plot_results(result, rank, title):
+def plot_results(result, rank, title, n_shots=1000):
     """
     Produces a bar chart for some input results basis states vs counts.
     Also takes a rank parameter r to show only show the r most common measurement results.
     """
+    
     sorted_experimental_results = dictionary_sorter(result.get_counts(), rank)
     sorted_result_keys = sorted_experimental_results.keys()
-    print(len(list(sorted_result_keys)))
     sorted_basis_states = list(map(tuple_to_string1, sorted_result_keys))
     sorted_count_values = list(sorted_experimental_results.values())
+    n_green_counts = sorted_count_values[0] # number of measurements in our "correct" state
+    
     fig = plt.figure()
     ax = fig.add_axes([0, 0, 2, 2])
     ax.bar(
@@ -45,4 +47,8 @@ def plot_results(result, rank, title):
         color=["green"] + (["orange"] * (len(sorted_count_values) - 1)),
     )
     ax.set_title(title)
+    plt.ylim([0, (0.65 * n_shots)])
+    plt.xlabel("Basis States")
+    plt.ylabel("Number of Shots")
+    print("Green counts:", n_green_counts, 'out of', n_shots, 'shots.')
     plt.show()
